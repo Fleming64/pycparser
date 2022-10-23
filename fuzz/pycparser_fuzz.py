4542d512ca4992c2b47d143a56e3e854b0716e2b
fleming64 <MSFleming64@gmail.com>
2022-10-22 22:16:22 -0700

    mayhem fix 8

--- fuzz/pycparser_fuzz.py
+++ fuzz/pycparser_fuzz.py
@@ -6,13 +6,18 @@ import os
 
 with atheris.instrument_imports():
     from pycparser import c_parser
+    from pycparser.plyparser import ParseError
 
 parser = c_parser.CParser()
 
 @atheris.instrument_func
 def TestOneInput(data):
-    fdp = atheris.FuzzedDataProvider(data)
-    parser.parse(fdp.ConsumeUnicodeNoSurrogates(len(data)))
+    try:
+        parser.parse(data.decode('utf8'))
+    except UnicodeDecodeError:
+        pass
+    except ParseError:
+        pass
 
 
 atheris.Setup(sys.argv, TestOneInput)
